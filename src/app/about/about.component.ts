744dd28a2e78d75a3f9077d2515fec8e7ef71489
André <allysonrodriguesx@gmail.com>
2025-06-06 00:24:39 -0300

    Tentativa de criação de animação

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  title = 'about';
+  som = ''
+
+  constructor(){
+    
+    
+
+  }
+
 }
+
+// Como manupular o arquivo typeScript no angular
+// Não estou sabendo por o código; O que deve ser feito para conseguir fazer o 'onclick' funcionar? -> deve ser criado um constructor ( não sei o porquê!? ); Vou pesquisar na documentação o que tem falando a respeito
+// Tudo que sei que para por os códigos precisa de um contructor(); No site não encontrei a explicação do uso explicitamente, só consegui descobrir por conta de vídeos
+// O que eu vou fazer para conseguir aprender angular pela própria documetação?
